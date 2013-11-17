2e4f690a26156a97b9f8ea1f4ea701dfb2109c33
Cesa salaam <cesaayastudent@gmail.com>
2013-11-17 17:58:51 -0500

    Cesa Salaam's BinarySearch
    This is another way to do Binary Search.

--- projects/bsearch/RealSeptimalsbseach.py
+++ projects/bsearch/RealSeptimalsbseach.py
@@ -1,4 +1,3 @@
-
 # Alston Clark
 
 def bsearch(list,search):
@@ -33,8 +32,31 @@ def bsearch(list,search):
     else:
       return mid
 
+#Cesa Salaam's bsearch
 
-    
+myList = [9,10,12,14]
+def bSearch (List, element):
+    bottom = 0
+    top = len(List)
+    middle = 0
+    if element < List[len(List)- 1] and element >= List[0]:
+        if top != 0:
+            while top >= bottom :
+                middle = (bottom+top)//2
+                if List[middle] == element:
+                  # When List[middle] is eual to the element the function stops and middle (the index that the element is in) is brought out of the function.
+                    return middle
+                elif List[middle] < element:
+                    bottom = middle + 1
+                else:
+                    top = middle - 1
+        else:
+            return str(-1)+  " Your element was not found in the list,sorry try again." 
+    else:
+        return str(-1) + " Your element was not found in the list, sorry try again."
+# This is my test list: myList = [9,10,12,14]
+# This is an example of how you would call the function: print bSearch(myList,9)
+
 
 
 
